2b69118de6593c472cbd8e0baf64bfb74ec17577
Fynn Borja <pewdiestuff@gmail.com>
2024-06-05 20:13:58 +0800

    Finished The Hero Section of Landing Page

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+
+const Landing = () => {
+    return(
+    <>
+        <div className="flex justify-center items-center h-screen pb-40">
+            <div>
+                <h1 className="text-8xl font-oswald text-white text-center">GLEAM</h1>
+                <h2 className="text-2xl font-pragati text-white text-center">Sincereley, Harvey, Angel, Maggins and Fynn</h2>
+            </div>
+        </div>
+    </>
+    )
+}
+export default Landing
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <div className="flex justify-around bg-transparent py-4 sticky top-10">
+            <p className="font-pragati text-white text-2xl">GIGS</p>
+            <p className="font-pragati text-white text-2xl">MERCH</p>
+            <p className="font-pragati text-white text-2xl">MUSIC</p>
+            <p className="font-pragati text-white text-2xl">ABOUT</p>
+        </div>
+    );
+}
+export default NavBar
