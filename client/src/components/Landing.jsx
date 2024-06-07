fe5580a075157dda8210e061f291b089ab5327dc
Fynn Borja <pewdiestuff@gmail.com>
2024-06-07 10:51:35 +0800

    Fixed Call to Action Button

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
           <div className="flex flex-col items-center">
             <h1 className="text-8xl font-oswald text-white text-center drop-shadow">GLEAM</h1>
             <h2 className="text-xl font-pragati text-white text-center drop-shadow">Sincerely, Harvey, Angel, Maggins, and Fynn</h2>
-            <button type="button" className="mt-4 border border-gray-300 font-pragati text-white bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 hover:bg-black hover:text-white hover:shadow-lg transition duration-200 ease-in-out">Get in Touch</button>
+            <button type="button" onClick={() => window.open("https://www.facebook.com/profile.php?id=100095078382694")} className="mt-4 border border-gray-300 font-pragati text-white bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 hover:bg-black hover:text-white hover:shadow-lg transition duration-200 ease-in-out">Get in Touch</button>
           </div>
         </div>
       </>
