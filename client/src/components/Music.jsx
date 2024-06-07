eb1d1671aef5c0639af08e5b907c0270839fe20c
Fynn Borja <pewdiestuff@gmail.com>
2024-06-07 15:21:32 +0800

    Changed Music List Navigation from Scroll to Button Click

--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import AudioPlayer from './AudioPlayer'; // Import the new AudioPlayer component
 
 const Music = () => {
+    const scrollRef = useRef(null);
     const itemsRef = useRef([]);
     const [audioSrc, setAudioSrc] = useState(null);
     const [currentSong, setCurrentSong] = useState({ title: '', subtitle: '' });
@@ -50,6 +51,26 @@ const Music = () => {
         };
     }, []);
 
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                top: 0,
+                left: -300,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                top: 0,
+                left: 300,
+                behavior: 'smooth'
+            });
+        }
+    };
+
     return (
         <div className="flex justify-center items-center min-h-screen my-96">
             <div className="flex-col gap-80 justify-center items-center">
@@ -60,12 +81,13 @@ const Music = () => {
                     OUR MUSIC
                 </h2>
                 <div className="relative w-full max-w-screen-lg flex justify-center items-center">
+                    <img onClick={scrollLeft} ref={el => itemsRef.current[1] = el} className='h-16 mb-12 cursor-pointer hidden xl:block 2xl:block' src="public/assets/left-arrow-svgrepo-com.svg" alt="" />
                     <div className="grid gap-16 grid-cols-1
                         md:grid-cols-2
                         lg:grid-cols-3
-                        xl:grid-cols-none xl:grid-flow-col xl:gap-24 xl:overflow-x-auto xl:overflow-y-hidden xl:scroll-smooth xl:w-full
-                        2xl:grid-cols-none 2xl:grid-flow-col 2xl:gap-24 2xl:overflow-x-auto 2xl:overflow-y-hidden 2xl:scroll-smooth 2xl:w-full"
-                        style={{ scrollSnapType: 'x mandatory' }}>
+                        xl:mx-4 xl:grid-cols-none xl:grid-flow-col xl:gap-12 xl:overflow-x-hidden xl:overflow-y-hidden xl:scroll-smooth xl:w-full
+                        2xl:mx-4 2xl:grid-cols-none 2xl:grid-flow-col 2xl:gap-12 2xl:overflow-x-hidden 2xl:overflow-y-hidden 2xl:scroll-smooth 2xl:w-full"
+                        style={{ scrollSnapType: 'x mandatory' }} ref={scrollRef}>
                         
                         {[
                             { src: "/assets/gleam-song-1.jpeg", title: "GLEAM", subtitle: "Back Hug" },
@@ -79,9 +101,9 @@ const Music = () => {
                                 <p className="text-center text-white font-pragati text-xl">{song.title}</p>
                                 <p className="text-center text-white font-pragati">{song.subtitle}</p>
                             </div>
-                        ))}
-                        
+                        ))} 
                     </div>
+                    <img onClick={scrollRight} ref={el => itemsRef.current[10] = el} className='h-16 mb-12 cursor-pointer transform -scale-x-100 hidden xl:block 2xl:block' src="public/assets/left-arrow-svgrepo-com.svg" alt="" />
                 </div>
                 {isPlayerVisible && (
                     <AudioPlayer
